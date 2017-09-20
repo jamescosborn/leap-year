eecc00ff0ad2823beddfc1aefe2bb820b3681a3a
James Osborn <jamescarlosborn@gmail.com>
2017-09-19 23:43:16 -0700

    add index.html, css and js directories and files, and first behavior spec.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var leapYear = function(year) {
+    return false;
+  };
+
+
+
+
+
+
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  })
+})
