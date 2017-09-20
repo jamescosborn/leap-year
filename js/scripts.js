62211b5a818904c560f1f0730883c974d8c1a2a5
James Osborn <jamescarlosborn@gmail.com>
2017-09-19 23:53:33 -0700

    add 3rd spec to back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   var leapYear = function(year) {
-    if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return false;
+    } else if (year % 4 === 0) {
       return true;
     } else {
-    return false;
+      return false;
     }
   };
   $("form#leap-year").submit(function(event) {
@@ -11,5 +13,5 @@ $(document).ready(function() {
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
-  })
-})
+  });
+});
